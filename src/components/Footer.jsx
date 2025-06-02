84765b8881426d05d8b69516c4fcda2349d3df54
aserrano523 <antonio.serrano523@comunidadunir.net>
2025-06-02 02:04:00 +0200

    Nombres de los miembros del equipo añadidos en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <h2>Miembros del equipo:</h2>
+        <h2>Miembros del equipo n.º 1:</h2>
         <ul>
-            <li>Javier García</li>
-            <li>Javier López</li>
-            <li>Javier Martínez</li>
-            <li>Javier Pérez</li>
-            <li>Javier Rodríguez</li>
+            <li>Javier Moyano Vizcaino</li>
+            <li>Rubén Rabanedo Aldonza</li>
+            <li>Adrián Sanz Contreras</li>
+            <li>Antonio Serrano Fernández</li>
+            <li>Jon Zarate Martínez</li>
         </ul>
     </div>
   )
