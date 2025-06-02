9a433fdec359eb0cb4642afc1d5e7bef878c3969
blbk <javiermoy@gmail.com>
2025-06-02 19:44:13 +0200

    inclusión de elemento footer en el componente footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        <h2>Miembros del equipo n.º 1:</h2>
-        <ul>
-            <li>Javier Moyano Vizcaino</li>
-            <li>Rubén Rabanedo Aldonza</li>
-            <li>Adrián Sanz Contreras</li>
-            <li>Antonio Serrano Fernández</li>
-            <li>Jon Zarate Martínez</li>
-        </ul>
-    </div>
+    <footer>
+      <div>
+          <h2>Miembros del equipo n.º 1:</h2>
+          <ul>
+              <li>Javier Moyano Vizcaino</li>
+              <li>Rubén Rabanedo Aldonza</li>
+              <li>Adrián Sanz Contreras</li>
+              <li>Antonio Serrano Fernández</li>
+              <li>Jon Zarate Martínez</li>
+          </ul>
+      </div>
+    </footer>
   )
 }
 
